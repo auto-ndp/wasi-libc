5abd41710bf5e8fd0f0ece90801a6abb4cd67828
Simon Shillaker <mail@simonshillaker.com>
2021-04-06 18:18:27 +0200

    Switch on memory shrinking

--- libc-bottom-half/sources/sbrk.c
+++ libc-bottom-half/sources/sbrk.c
@@ -3,8 +3,16 @@
 #include <errno.h>
 #include <__macro_PAGESIZE.h>
 
+#ifdef __faasm
+    extern void* __sbrk(intptr_t increment);
+#endif
+
 // Bare-bones implementation of sbrk.
 void *sbrk(intptr_t increment) {
+#ifdef __faasm
+    // In Faasm we defer to our custom sbrk implementation
+    return __sbrk(increment);
+#else 
     // sbrk(0) returns the current memory size.
     if (increment == 0) {
         // The wasm spec doesn't guarantee that memory.grow of 0 always succeeds.
@@ -16,8 +24,8 @@ void *sbrk(intptr_t increment) {
         abort();
     }
 
-    // WebAssembly doesn't support shrinking linear memory.
     if (increment < 0) {
+        // WebAssembly normally doesn't support shrinking linear memory.
         abort();
     }
 
@@ -29,4 +37,5 @@ void *sbrk(intptr_t increment) {
     }
 
     return (void *)(old * PAGESIZE);
+#endif
 }
